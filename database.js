56d82894814faa4c5ef486773bc41679a1a05249
osyra42 <22022820+osyra42@users.noreply.github.com>
2023-07-05 16:25:08 +0000

    update to database

--- database.js
+++ database.js
@@ -1,7 +1,165 @@
-let database = '{"data":[{
-    "index": "#0001",
-    "timestamp": "June 27th, 2023",
-    "category": "Python",
-    "project": "Vanity"
-  }]
-}'
+var database =
+  [
+    
+{
+    "index": 22,
+    "timestamp": 1688574137454,
+    "category": "Media",
+    "project": "Book",
+    "details": `writing a book in code is pretty hard so i will be writing the book on Microsoft Word and exporting it to PDF from now on`
+},
+    {
+      "index": 21,
+      "timestamp": 1688574092993,
+      "category": "Javascript",
+      "project": "Logbook",
+      "details": `HUGE UPDATE !!\n- there is now a database to store all updates\n- log updates can be shared with a link now\n- timestamps now run on epoch time\n- auto formatting is now available for logging gui\n\nunfortunately the only observable change for visitors is the new date format`
+    },
+    {
+      "index": 20,
+      "timestamp": 1688184000000,
+      "category": "Javascript",
+      "project": "Logbook",
+      "details": `Added a form that would format updates for me into code, it's pretty cool and much faster to do it this way.\n\nAdded a dictionary look up table for colors`
+    },
+    {
+      "index": 19,
+      "timestamp": 1688184000000,
+      "category": "Javascript",
+      "project": "Book",
+      "details": `Started writing my book again, I can't find the previous versions so I will do my best to write it down from memory. If you are interested in reading it the file is located here --> <a href="https://book--osyra42.repl.co/">https://book--osyra42.repl.co/</a>`
+    },
+    {
+      "index": 18,
+      "timestamp": 1687838400000,
+      "category": "Javascript",
+      "project": "Logbook",
+      "details": `Added a form that would format updates for me into code, it's pretty cool and much faster to do it this way.\n\nAdded a dictionary look up table for colors`
+    },
+    {
+      "index": 17,
+      "timestamp": 1687752000000,
+      "category": "Service",
+      "project": "PC Repair",
+      "details": `- collection of PC repair programs called PC FIxIT\n- restored the project\n- this project's intent is the repair any pc using just the flash drive the project is on`
+    },
+    {
+      "index": 16,
+      "timestamp": 1687752000000,
+      "category": "Hardware",
+      "project": "Star Drive",
+      "details": `- built a new flash drive\n- named it Star Drive with code and made it have a persistant icon\n- this flash drive will combine the functions of 9 other flash drives`
+    },
+    {
+      "index": 15,
+      "timestamp": 1687752000000,
+      "category": "Javascript",
+      "project": "Logbook",
+      "details": `- added a discription section to explain this webpage\n- changed the default color of the summaries to black\n- collected all the colors to a part of the top of the script\n- preped the javascript to use json notation to add sections`
+    },
+    {
+      "index": 14,
+      "timestamp": 1687752000000,
+      "category": "Community",
+      "project": "Discord",
+      "details": `Converted the Discord guild to a community to accomidate the verification of Vanity.`
+    },
+    {
+      "index": 13,
+      "timestamp": 1687752000000,
+      "category": "Python",
+      "project": "vanity_py",
+      "details": `added a sample slash command to verify the bot`
+    },
+    {
+      "index": 12,
+      "timestamp": 1687665600000,
+      "category": "Javascript",
+      "project": "Logbook",
+      "details": `- updated deployment location to <a href="https://osyra42.github.io">https:\//osyra42.github.io</a>\n- removed <span>-code</span> from all categories and use full language name\n- added proper case to all summary elements `
+    },
+    {
+      "index": 11,
+      "timestamp": 1687406400000,
+      "category": "Community",
+      "project": "Discord",
+      "details": `Discord server activity xp has been reset, will be reset occasionally.`
+    },
+    {
+      "index": 10,
+      "timestamp": 1687320000000,
+      "category": "Javascript",
+      "project": "Logbook",
+      "details": `Logbook now has color! This is the color update where logs are color coded based on project name. Only read what you want, skip what you don't want to read.`
+    },
+    {
+      "index": 9,
+      "timestamp": 1687233600000,
+      "category": "Javascript",
+      "project": "Logbook",
+      "details": `project rename\n<span>event-logs --> logbook</span>\n- made updates reflect on github`
+    },
+    {
+      "index": 8,
+      "timestamp": 1687147200000,
+      "category": "Javascript",
+      "project": "Logbook",
+      "details": `- adjusted colors to enable new title bar colors\n- fixed a width issue when viewing on desktop\n- fixed a visual issue with borders`
+    },
+    {
+      "index": 7,
+      "timestamp": 1687060800000,
+      "category": "Hardware",
+      "project": "vanity_py",
+      "details": `purchased a new ribbon cable lock for raspberry pi display board for 7" touch display`
+    },
+    {
+      "index": 6,
+      "timestamp": 1687060800000,
+      "category": "Javascript",
+      "project": "Logbook",
+      "details": `complete overhaul of the project, including simpler html, removal of the table, and a new color scheme. the new layout will allow for much more detailed logs. unfortunately the sort feature was removed but finding what you need is as easy as using ctrl+f on your browser`
+    },
+    {
+      "index": 5,
+      "timestamp": 1686628800000,
+      "category": "Javascript",
+      "project": "Logbook",
+      "details": `project name changed\n<span>project_notes --> event-logs</span>\nupdated the colors of the theme\nmade colors a variable to make changes easier\nset up a system to let logs be much longer\nwill implement openable logs soon`
+    },
+    {
+      "index": 4,
+      "timestamp": 1686542400000,
+      "category": "Javascript",
+      "project": "Logbook",
+      "details": `cleaned up table code for better background processing`
+    },
+    {
+      "index": 3,
+      "timestamp": 1686542400000,
+      "category": "Javascript",
+      "project": "Logbook",
+      "details": `improved theme for better readability`
+    },
+    {
+      "index": 2,
+      "timestamp": 1686456000000,
+      "category": "Javascript",
+      "project": "Logbook",
+      "details": `created a sortable table for taking notes on projects`
+    },
+    {
+      "index": 1,
+      "timestamp": 1686196800000,
+      "category": "Python",
+      "project": "vanity_py",
+      "details": `restarted code for the discord bot`
+    },
+    {
+      "index": 0,
+      "timestamp": 649396800,
+      "category": "Jokes",
+      "project": "Project",
+      "details": `You're a wizard Harry!`
+    },
+  ]
