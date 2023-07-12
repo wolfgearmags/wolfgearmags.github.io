d66e1472dd9c2c6e9d03bbed543a60655c8c787d
osyra42 <22022820+osyra42@users.noreply.github.com>
2023-07-12 01:17:45 +0000

    adding hds to this page

---
+++ hds/nav.php
@@ -0,0 +1,26 @@
+<!-- Header -->
+<header id="header">
+        <h1 id="logo"><a href="index.php">Heart Dab Studio</a></h1>
+        <nav id="nav">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="shop.php">Shop</a></li>
+                <li>
+                    <a href="#">Services</a>
+                    <ul>
+                        <li><a href="free_things.php">Free Things</a></li>
+                        <li><a href="self_help.php">Self Help</a></li>
+                        <hr style="padding:0px;margin:0px;">
+                        <li><a href="3d_prints.php">3D Prints</a></li>
+                        <li><a href="vtuber_models.php">Vtuber Model</a></li>
+                        <li><a href="computer_repair.php">Computer Repair</a></li>
+                        <li><a href="photo_editing.php">Photo Editing (soon)</a></li>
+                        <li><a href="video_editing.php">Video Editing (soon)</a></li>
+                        <li><a href="3d_modeling.php">3D Modeling (soon)</a></li>
+                    </ul>
+                </li>
+                <li><a href="contact_me.php">Contact Me</a></li>
+                <li><a target="_blank" href="https://paypal.me/heartdabstudio" class="button primary">Donate</a></li>
+            </ul>
+        </nav>
+    </header>
